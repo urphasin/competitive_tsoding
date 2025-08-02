ec4a9fbbe92c1b58ca22a7e4426e4b4dc9ff8026
Otito Amuga <urphasin@gmail.com>
2025-08-01 22:45:25 -0600

    mi casa es su casa

---
+++ template.cpp
@@ -0,0 +1,30 @@
+#include <bits/stdc++.h>
+
+#define all(x) (x).begin(), (x).end()
+
+using namespace std;
+
+inline int nxt() {
+  int x;
+  cin >> x;
+  return x;
+}
+
+void solve() {
+
+}
+
+int main() {
+  ios_base::sync_with_stdio(false);
+  cin.tie(nullptr);
+
+  // freopen("input.txt", "r", stdin);
+  // freopen("output.txt" "w", stdout);
+
+  int t = 1; // nxt();
+  while(t--) {
+    solve();
+  }
+
+  return 0;
+};
