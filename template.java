6c40b672bad77e824f16fee717a54c00002ba7bc
Otito Amuga <urphasin@gmail.com>
2025-08-03 16:55:35 -0600

    Java competitive coding template

---
+++ template.java
@@ -0,0 +1,28 @@
+package CSES.template;
+
+import java.io.*;
+import java.util.*;
+import java.lang.Math;
+
+public class template {
+
+    static BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void solve() throws IOException {
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+//        int n = Integer.parseInt(st.nextToken());
+    }
+
+    public static void main(String[] args) throws IOException {
+        int t = 1; // Integer.parseInt(br.readLine());
+
+        while (t-- > 0) {
+            solve();
+        }
+
+        bw.flush();
+        bw.close();
+        br.close();
+    }
+}
